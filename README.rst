f0e22d33a95623e637fb53c777d7664442379292
Riccardo Attilio Galli <riccardo@sideralis.org>
2012-07-14 20:22:40 +0200

    First Commit

---
+++ README.rst
@@ -0,0 +1,5 @@
+A standalone validator/filters library.
+Works either as
+- a nodeJS module
+- an AMD module
+- a generic library
---
+++ validator.js
@@ -0,0 +1,36 @@
+/**
+ * Copyright 2012 Riccardo Attilio Galli
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+(function (root, factory) {
+    if (typeof exports === 'object') {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like enviroments that support module.exports,
+        // like Node.
+        module.exports = factory();
+    } else if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define([], factory);
+    } else {
+        // Browser globals
+        root.validator = factory();
+    }
+}(this, function (/*dependencies*/) {
+    return {
+        
+        
+        
+    };
+}));
