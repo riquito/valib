1f1b1e58f502bfb55219e1398a0987c0f9535d93
Riccardo Attilio Galli <riccardo@sideralis.org>
2012-07-15 22:13:13 +0200

    Added e-mail and url to copyright informations

--- validator.js
+++ validator.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2012 Riccardo Attilio Galli
+ * Copyright 2012 Riccardo Attilio Galli <riccardo@sideralis.org> [http://www.sideralis.org]
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
