88cf07ab264b95506d2cc122549ac2eb0062c631
Riccardo Attilio Galli <riccardo@sideralis.org>
2014-05-01 15:52:54 +0200

    Released version 2.0.0

--- bower.js
+++ bower.js
@@ -1,11 +1,10 @@
 {
  "name": "valib",
- "version": "1.0.0",
+ "version": "2.0.0",
  "main": "valib.js",
  "keywords": ["validation", "amd", "browser", "server", "client", "util"],
- "ignore" : [".*", "Makefile", "NOTICE", "README*", "package.json", "test/*"],
+ "ignore" : [".*", "Makefile", "NOTICE", "CHANGELOG", "README*", "package.json", "test/*"],
  "devDependencies": {
-   "chai" : "1.x",
    "mocha" : "1.x"
  }
 }
